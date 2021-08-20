da476e3695d3d349a3e5d22d44f84776c78a1b60
Cariny Perez <perezcariny@gmail.com>
2021-08-20 16:55:04 -0700

    Add a character componennt

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import obi from './assets/r5d4.glb'
 import React, {useEffect, useState} from 'react'; 
-import Character from './components/character/character.component'
 import Homepage from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom'; 
 import Galaxy from './pages/galaxy/galaxy.component'; 
 
 
 function App() {
-  const [data, setData] = useState(); 
-  const apiRequest = () => {
-    const url = '/api/characters';
-    console.log(url);
-    fetch(url).then(response => response.json())
-    .then(response => setData(response.results)) 
-    .catch(err => console.log(err))
-  }
-  useEffect(() =>  {
-    // apiRequest(); 
-  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -27,12 +14,6 @@ function App() {
          <Route exact path ='/' component={Homepage}></Route>
          <Route exact path ='/galaxy' component={Galaxy}></Route>
        </Switch>
-        {/* <model-viewer src={obi} 
-        alt="A 3D model of a robot" 
-        auto-rotate="" camera-controls="" 
-        background-color="#455A64"></model-viewer> */}
-        {/* <model-viewer src={soldier} alt="A 3D model of a robot" auto-rotate="" camera-controls="" background-color="#455A64"></model-viewer> */}
-        {/* <Character data={data}/> */}
       </header>
     </div>
   );
--- client/src/pages/galaxy/galaxy.component.jsx
+++ client/src/pages/galaxy/galaxy.component.jsx
@@ -2,22 +2,36 @@ import galaxy from '../../assets/galaxy.jpeg';
 import stars from '../../assets/stars.jpeg';
 import './galaxy.styles.scss'; 
 import {useState, useEffect} from 'react';
+import Character from '../../components/character/character.component.jsx'
 
 const Galaxy = () => {
+    const [data, setData] = useState(); 
     const [image, setImage] = useState(stars);
     const [text, setText]  = useState(''); 
+
     const switchImage = () => {
         setText(''); 
         setImage(galaxy);
     }
+
+    const apiRequest = () => {
+      const url = '/api/characters';
+      console.log(url);
+      fetch(url).then(response => response.json())
+      .then(response => setData(response.results)) 
+      .catch(err => console.log(err))
+    }
+
     useEffect(() => {
         setText('A long time ago, in a galaxy far, far away ...')
-        setInterval(switchImage, 5000); 
+        setInterval(switchImage, 5000);
+        setInterval(apiRequest, 5000);
     }, [])
     return (
         <div className='galaxy'>
             <h1>{text}</h1>
             <img className='stars-image' src={image} alt='stars'></img>
+            <Character data={data}/>
         </div>
     )
 }
